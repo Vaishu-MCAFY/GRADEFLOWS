4cb6f89c4b9edb3fb9021f46c27ec2239856867e
Vaishu-MCAFY <akangirevaishnavi2003@gmail.com>
2026-07-20 23:44:11 +0530

    Add files via upload

---
+++ gradeflow/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # Student App
+    path("", include("students.urls")),
+]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
